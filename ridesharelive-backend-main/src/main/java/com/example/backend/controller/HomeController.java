bfcfe583ae0aeb9228f59797b8594e44f965c19f
Sumit Dhondikar <sumitdhon09@gmail.com>
2026-04-05 00:23:35 +0530

    added home controller

---
+++ ridesharelive-backend-main/src/main/java/com/example/backend/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "RideShare backend is running 🚀";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
